28791d3e3b0283a4db3a577cfc0d4f7fa14ea9fb
LUIS F. URBINA MORENO <LUIZ-FELIPE16@HOTMAIL.COM>
2019-10-26 18:19:28 -0500

    Initial commit

---
+++ complete/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -0,0 +1,7 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+public class WebMvcConfig {}
---
+++ complete/src/main/java/com/example/demo/config/WebSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.demo.config;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("test").password("password").roles("USER");
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().httpStrictTransportSecurity().xssProtection()
+                .and().authorizeRequests().anyRequest().fullyAuthenticated()
+                .and().csrf().disable();
+
+        http.httpBasic().authenticationEntryPoint(authenticationEntryPoint());
+        http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
+
+    }
+}
